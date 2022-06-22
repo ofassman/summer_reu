4e1bca9a19aef32c5f679a8e6e53d79bd13abdd4
Ellie Fassman <oef3@cornell.edu>
2022-06-22 12:10:37 -0500

    started abc

---
+++ abc.py
@@ -0,0 +1,42 @@
+import elfi
+import growtree
+import matplotlib.pyplot as plt
+import scipy.stats as ss
+import numpy as np
+
+birth = elfi.Prior('uniform', 0, 5)
+death = elfi.Prior('uniform', 0, 5)
+sub = elfi.Prior('uniform', 0, 5)
+
+
+def gen_tree_sims(b, d, s):
+    t = growtree.gen_tree(b,d,s,100,1,1,1,0,100)
+    # generate stats for t?
+    return t
+
+# true parameters
+birth_true = 1.3
+death_true = 2.5
+sub_true = 1
+
+y_obs = gen_tree_sims(birth_true, death_true, sub_true)
+
+Y = elfi.Simulator(gen_tree_sims, birth, death, sub, observed=y_obs)
+
+
+def treestat(tree):
+    return growtree.tree_height(tree)
+
+S1 = elfi.Summary(treestat, Y)
+S2 = elfi.Summary(treestat, Y) 
+# Finish the model with the final node that calculates the squared distance (S1_sim-S1_obs)**2 + (S2_sim-S2_obs)**2
+d = elfi.Distance('euclidean', S1, S2)
+rej = elfi.Rejection(d, batch_size=10000)
+N = 1000
+
+vis = dict(xlim=[-2,2], ylim=[-1,1])
+
+# You can give the sample method a `vis` keyword to see an animation how the prior transforms towards the
+# posterior with a decreasing threshold.
+result = rej.sample(N, quantile=0.01, vis=vis)
+result.summary()
