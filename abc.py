80416362d47a574ad26e6a24f6aff09b793dce17
Ellie Fassman <oef3@cornell.edu>
2022-06-23 23:26:08 -0500

    modifying abc, still buggy

--- abc.py
+++ abc.py
@@ -5,15 +5,15 @@ import scipy.stats as ss
 import numpy as np
 import scipy
 
-
-
-def gen_tree_sims(b, d, n_sims=1):
+def gen_tree_sims(b=1, d=1, batch_size=1, random_state=None):
     arr = []
-    for i in range(0, n_sims):
+    random_state = random_state or np.random
+    for i in range(0, batch_size):
         arr.append(growtree.gen_tree(b,d,1,100,1,1,1,0,100))
     return arr
+
 def treestat(tree):
-    return growtree.tree_height(tree)
+    return growtree.tree_height(tree[0])
 
 # true parameters
 birth_true = 1.3
@@ -23,24 +23,14 @@ sub_true = 1
 birth = elfi.Prior(scipy.stats.uniform, 0, 5)
 death = elfi.Prior(scipy.stats.uniform, 0, 5)
 
-#y_obs = gen_tree_sims(birth_true, death_true, sub_true)
-Y = elfi.Simulator(gen_tree_sims, birth_true, death_true, 1)
-X = elfi.Simulator(gen_tree_sims, birth, death, 1)
+obs = (gen_tree_sims(birth_true, death_true, sub_true))[0]
+Y = elfi.Simulator(elfi.tools.vectorize(gen_tree_sims),birth,death,observed=obs)
 
 S1 = elfi.Summary(treestat, Y)
-S2 = elfi.Summary(treestat, X)
-d = elfi.Distance('euclidean', S1, S2)
-rej = elfi.Rejection(d, batch_size=10000)
 
+dist = elfi.Distance('euclidean', S1)
 
-"""
-N = 1000
+rej = elfi.Rejection(dist, batch_size=10000)
 
-vis = dict(xlim=[-2,2], ylim=[-1,1])
-
-# You can give the sample method a `vis` keyword to see an animation how the prior transforms towards the
-# posterior with a decreasing threshold.
-result = rej.sample(N, quantile=0.01, vis=vis)
+result = rej.sample(10, threshold=0.2)
 result.summary()
-
-"""
